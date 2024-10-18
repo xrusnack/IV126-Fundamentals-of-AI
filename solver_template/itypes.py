e2e090f1e977e7928812689c4e48f9028fa7f5be
Jakub Čillík <524749@mail.muni.cz>
2024-10-18 11:47:01 +0200

    Typo

--- solver_template/itypes.py
+++ solver_template/itypes.py
@@ -1,11 +1,11 @@
 from typing import Dict, List, Literal
 
-type Root = Literal["Coodinates"] \
+Root = Literal["Coordinates"] \
     | Literal["Matrix"] \
     | Literal["GlobalBest"] \
     | Literal["GlobalBestVal"] \
     | Literal["Timeout"]
 
-type Data = List[List[int]] | List[int] | int
+Data = List[List[int]] | List[int] | int
 
-type Instance = Dict[Root, Data]
+Instance = Dict[Root, Data]
