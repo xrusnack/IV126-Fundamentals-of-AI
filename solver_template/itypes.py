d59be571b33941971719050c0929cd63674fcf01
Jakub Čillík <524749@mail.muni.cz>
2024-10-18 11:27:45 +0200

    Added type annotation for instances

---
+++ solver_template/itypes.py
@@ -0,0 +1,10 @@
+from typing import Dict, List, Literal
+
+type Root = Literal["Coodinates"] \
+    | Literal["GlobalBest"] \
+    | Literal["GlobalBestVal"] \
+    | Literal["Timeout"]
+
+type Data = List[List[int]] | List[int] | int
+
+type Instance = Dict[Root, Data]
